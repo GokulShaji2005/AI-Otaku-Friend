9fa83f9ac806ed7937e106e5f4916a3c1a47454e
Gokul Shaji <gokulshaji2005@gmail.com>
2025-07-19 09:25:00 +0530

    Auto scroll and msg alignment done

--- Frontend/Client/src/pages/Chat.jsx
+++ Frontend/Client/src/pages/Chat.jsx
@@ -26,6 +26,8 @@ const Chat = () => {
       const [inputMessage,setInputMessage]=useState("");
       const userMessageRef=useRef([]);
       const [messageArrays,setMessageArrays]=useState([]);
+      const ScrollRef=useRef(null);
+
      useEffect(() => {
         const msgFromBackend=(data)=>{
         const AiMsgRef={sender:null,ai:data};
@@ -40,24 +42,27 @@ const Chat = () => {
     
       }, [])
       const sendMessage=()=>{
-        if(inputMessage.trim() === '')return;
+        if(inputMessage.trim() === '')
+          {
+            return;
+          }
+
         socket.emit("chatFromUser",inputMessage);
+
         setInputMessage(inputMessage)
-     
-
-
        const useMsgRef={sender:inputMessage,ai:null};
        userMessageRef.current.push(useMsgRef);
        setMessageArrays([...userMessageRef.current])
-       
-        
-      
        setInputMessage('')
 
-
-  
-     
       }
+      const chatBottomRef=()=>{
+         ScrollRef.current?.scrollIntoView({behavior:'smooth'})
+      };
+
+      useEffect(()=>{
+        chatBottomRef()
+      },[messageArrays])
      
   return (
     
@@ -73,21 +78,24 @@ const Chat = () => {
             </div>
           </div>
         </nav>
-        <div className='flex-grow flex justify-center items-center mb-8 '>
-          <div className='md:h-[80vh] max-w-2xl bg-gradient-to-br from-[#1e3c72] to-[#2a5298] md:w-[85vw]   p-5 rounded-2xl shadow-2xl flex flex-col justify-between border border-white/20'>
+        <div className='flex-grow flex justify-center items-center mb-10 '>
+          <div className='md:h-[85vh]  bg-gradient-to-br from-[#1e3c72] to-[#2a5298] md:w-[80vw]   p-5 rounded-2xl shadow-2xl flex flex-col justify-between border border-white/20'>
             <h2 className="text-3xl font-bold mb-2 text-white text-center">Otaku AI Chat</h2>
-            <div className="flex-1 overflow-y-auto mb-6 rounded-lg bg-white/10 p-4">
+            <div className="flex-1 overflow-y-auto mb-6  rounded-lg bg-white/10 p-4" 
+            
+            >
               {messageArrays && (
                 messageArrays.map((msg, index) => (
-                  <div className='font-thin text-white' key={index}>
+                  <div className={msg.sender?'flex justify-end font-thin text-white':'font-thin text-white'} key={index}
+                     ref={ScrollRef}>
                     {msg.sender && (
-                      <div className="mb-2 p-3 rounded-lg text-sm bg-white/50 text-white/80  w-fit max-w-[70%] break-words">{msg.sender}
+                      <div className="mb-6 p-3 rounded-lg text-sm bg-white/50 text-white/80  w-fit max-w-[60%] break-words">{msg.sender}
                     </div>
                     )}
-                    {msg.ai && ( <div className="mb-2 p-3 rounded-lg text-sm bg-white/20 text-white w-fit max-w-[80%] break-words ">
+                    {msg.ai && ( <div className="mb-6 p-3 rounded-lg flex justify-end text-sm bg-white/20 text-white w-fit max-w-[80%] break-words ">
                       <span className="font-semibold text-[#D8F4F6]  ">AI:</span> {msg.ai}
                     </div>)}
-                   
+                
                   </div>
                 ))
               )}
